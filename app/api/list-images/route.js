78e1586be9515a97e69390a44752c48d6d4ef521
ChandupaSH <chandupash107@gmail.com>
2025-08-04 16:12:39 +0530

    adding new list pages

--- app/api/list-images/route.js
+++ app/api/list-images/route.js
@@ -1,9 +1,23 @@
-import fs from "fs/promises";
-import path from "path";
 import { NextResponse } from "next/server";
+import cloudinary from "cloudinary";
+
+cloudinary.v2.config({
+  cloud_name: "dwfqbpxfb",
+  api_key: "564997124141877",
+  api_secret: "A_1m_eF4Mqr90gBeRoUj4Ll4EsQ",
+});
 
 export async function GET() {
-  const dir = path.join(process.cwd(), "public", "data", "images");
-  const files = await fs.readdir(dir);
-  return NextResponse.json({ files });
+  try {
+    const result = await cloudinary.v2.api.resources({
+      type: "upload",
+      prefix: "images/", // Only images in the "images" folder
+      resource_type: "image",
+    });
+
+    const urls = result.resources.map((file) => file.secure_url);
+    return NextResponse.json({ files: urls });
+  } catch (err) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
 }
