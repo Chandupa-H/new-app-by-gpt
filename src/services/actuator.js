9fea4b4fb43a47bbab9c8d71768aa1db98624077
ChandupaSH <chandupash107@gmail.com>
2025-08-14 15:26:07 +0530

    add actuator IP

--- src/services/actuator.js
+++ src/services/actuator.js
@@ -1,7 +1,7 @@
 // src/services/esp32Api.js
 
 // HARD-CODED ESP32 IP
-const ESP32_IP = "172.25.249.214"; // <-- Change to your ESP32 IP
+const ESP32_IP = "172.25.249.22"; // <-- Change to your ESP32 IP
 const BASE_URL = `http://${ESP32_IP}`;
 
 export const sendActuatorCommand = async (motor, direction) => {
